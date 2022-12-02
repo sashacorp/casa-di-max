afb71448845f3494ae9465cc2870649def6bf82b
muni <booscarface@gmail.com>
2022-12-02 18:10:23 +0100

    added donations

--- src/templates/donationTemplate.js
+++ src/templates/donationTemplate.js
@@ -4,15 +4,12 @@ import Layout from '../components/layout'
 export default function Template ({ pageContext }) {
     const { product } = pageContext
     return (
-        <>
-      <MDXProvider components={shortcodes}>
        <Layout>
        <h1>{product.title}</h1>
        <div>{product.description}</div>
-       {children}
+       
        </Layout>
-      </MDXProvider>
-    </>
+    
     )
 }
 
