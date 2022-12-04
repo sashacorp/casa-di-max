08308746e7014a705408594e25868bfb0152a8a2
muni <booscarface@gmail.com>
2022-12-04 10:19:39 +0100

    design sperimentations

--- src/components/welcomesection.js
+++ src/components/welcomesection.js
@@ -1,13 +1,10 @@
 import * as React from 'react'
-import Landing from '../images/casadimaxLanding.svg'
+import Landing from '../images/casadimaxLanding.jpg'
 import '../styles/styles.scss'
 
 const WelcomeSection = () => {
    return (
       <div className="WelcomeSection">
-         <div className="Title">
-         <h1>Casa di Max</h1>
-         </div>
          <div className="Media">
          <img src={Landing} alt="landing"/>
          </div>
