3ac7e06d618bce2673cbc19f471b66e6c3acd23f
muni <booscarface@gmail.com>
2022-12-05 11:52:23 +0100

    navbar hook didn't work

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react"
+import React from "react"
 import { Link } from "gatsby"
 import '../styles/styles.scss'
 import Logo from '../images/logocasadimax.svg'
@@ -7,33 +7,15 @@ import Logo from '../images/logocasadimax.svg'
 
 function NavBar () {
   
-  const [toggleMenu, setToggleMenu] = useState(false)
   // I tried to solve the global variable problem without passing it into useState() but I'm pretty sure it doesn't make any sense.
-  const [screenWidth, setScreenWidth ] = useState()
-
-  const toggleNav = () => {
-    setToggleMenu(!toggleMenu)
-  }
-  
-  useEffect(() => {
-    const changeWidth = () => {
-      setScreenWidth(window.innerWidth);
-    }
-      window.addEventListener('resize', changeWidth);
-
-    return () => {
-      window.removeEventListener('resize', changeWidth)
-    }
-  }, [])
+ 
   return (
     <div className="navigation">
       <div className="Logo">
-        <Link to="/" onClick={toggleNav} className="img-link">
+        <Link to="/" className="img-link">
         <img src={Logo} alt="logo"/>
         </Link>
       </div>
-      {
-        (toggleMenu || screenWidth > 700 ) && (
           <ul>   
               <li key="home"><Link to="/">Home</Link></li>
               <li key="about"><Link to="/aboutus">About</Link></li>
@@ -43,8 +25,6 @@ function NavBar () {
               <li key="donate"><Link to="/donate">Donate</Link></li>
               <li key="contact"><Link to="/contact">Contacts</Link></li>
           </ul>
-        )
-      }
     </div>
   )
 }
