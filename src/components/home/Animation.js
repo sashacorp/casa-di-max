6c8004878b1da5aaadee1274167d3e86d1494cf1
muni <booscarface@gmail.com>
2023-03-07 14:21:45 +0100

    animation breakpoints

--- src/components/home/Animation.js
+++ src/components/home/Animation.js
@@ -3,30 +3,75 @@ import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax'
 import cloud from '../../images/cloud.png'
 import earth from '../../images/earth.png'
 import littleDog from '../../images/littledog.png'
-import Max from '../../images/max.png'
-import Onlus from '../../images/onlus.png'
-import Arzana from '../../images/arzana.png'
-import SardinienHunde from '../../images/sardinienhunde.png'
+// import Max from '../../images/max.png'
+// import Onlus from '../../images/onlus.png'
+// import Arzana from '../../images/arzana.png'
+// import SardinienHunde from '../../images/sardinienhunde.png'
+import { Typography } from '@material-ui/core'
+// import { Grid } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { useMediaQuery } from '@mui/material'
 
+const useStyles = makeStyles(theme => ({
+   responsiveText: {
+    [theme.breakpoints.down('md')]: {
+      fontSize: '3rem'
+    },
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '2rem'
+    },
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '1.5rem'
+    },
+   },
+   responsiveImage: {
+    width: '20%',
+    [theme.breakpoints.down('md')]: {
+      width: '30%'
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: '50%'
+    },
+    [theme.breakpoints.down('xs')]: {
+      width: '70%'
+    },
+   },
+   responsiveEarth: {
+    width: '60%',
+    [theme.breakpoints.down('md')]: {
+      width: '70%'
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: '80%'
+    },
+    [theme.breakpoints.down('xs')]: {
+      width: '90%'
+      
+    },
+   }
+}))
 // Little helpers ...
 const url = (name, wrap = false) =>
   `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
 
 
 
+
 export default function Animation() {
   const parallax = useRef((null))
-
+  const classes= useStyles()
+  
+  
   return (
     <div style={{ width: '100%', height: '100%', background: '#253237' }}>
-      <Parallax ref={parallax} pages={3}>
+      <Parallax ref={parallax} pages={6}>
         <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#253237' }} />
         <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#253237' }} />
 
         <ParallaxLayer
           offset={0}
           speed={0}
-          factor={3}
+          factor={6}
           style={{
             backgroundImage: url('stars', true),
             backgroundSize: 'cover',
@@ -34,7 +79,7 @@ export default function Animation() {
         />
 
         <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
-          <img src={url('satellite4')} style={{ width: '15%', marginLeft: '70%' }} />
+          <img src={url('satellite4')} style={{ width: '15%', marginLeft: '80%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
@@ -70,7 +115,7 @@ export default function Animation() {
         </ParallaxLayer>
 
         <ParallaxLayer
-          offset={2.5}
+          offset={5.5}
           speed={-0.4}
           style={{
             display: 'flex',
@@ -78,7 +123,7 @@ export default function Animation() {
             justifyContent: 'center',
             pointerEvents: 'none',
           }}>
-          <img src={earth} style={{ width: '60%' }} />
+          <img src={earth} className={classes.responsiveEarth} />
         </ParallaxLayer>
 
         {/* <ParallaxLayer
@@ -90,41 +135,94 @@ export default function Animation() {
             backgroundImage: `url(${Max})`,
           }}
         /> */}
-{/* 
+
         <ParallaxLayer
           offset={0}
+          speed={0.1}
+          onClick={() => parallax.current.scrollTo(0)}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          
+              <Typography variant="h1" style={{color: 'rgba(255, 213, 111, 0.8)'}} classes={{h1: classes.responsiveText}}> 
+                Benvenuti al rifugio Casa di Max!
+              </Typography>
+            
+          
+        </ParallaxLayer> 
+
+        <ParallaxLayer
+          offset={1}
           speed={0.1}
           onClick={() => parallax.current.scrollTo(1)}
           style={{
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
+            
           }}>
-          <img src={url('server')} style={{ width: '20%' }} />
-        </ParallaxLayer> */}
+          <Typography variant="h1" style={{color: 'rgba(255, 213, 111, 0.8)', width: '70%'}} classes={{h1: classes.responsiveText}}> 
+          Max è stato un cagnolino molto speciale, tanto da averci ispirato a fondare un rifugio in sua memoria!
+              </Typography>
+        </ParallaxLayer> 
 
-        {/* <ParallaxLayer
-          offset={1}
+        <ParallaxLayer
+          offset={2}
           speed={0.1}
           onClick={() => parallax.current.scrollTo(2)}
           style={{
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
+            
           }}>
-          <img src={url('bash')} style={{ width: '40%' }} />
-        </ParallaxLayer> */}
+          <Typography variant="h1" style={{color: 'rgba(255, 213, 111, 0.8)', width: '70%'}}  classes={{h1: classes.responsiveText}}> 
+          Barbara Pistis ha istituito la Onlus ConFidointe a tutela dei pet meno fortunati d'Europa.
+              </Typography>
+        </ParallaxLayer> 
+
+        <ParallaxLayer
+          offset={3}
+          speed={0.1}
+          onClick={() => parallax.current.scrollTo(2)}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            
+          }}>
+          <Typography variant="h1" style={{color: 'rgba(255, 213, 111, 0.8)', width: '70%'}}  classes={{h1: classes.responsiveText}}> 
+          Il rifugio Casa di Max è una struttura attiva nelle campagne ogliastrine, in Sardegna.
+              </Typography>
+        </ParallaxLayer> 
+
+        <ParallaxLayer
+          offset={4}
+          speed={0.1}
+          onClick={() => parallax.current.scrollTo(2)}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            
+          }}>
+          <Typography variant="h1" style={{color: 'rgba(255, 213, 111, 0.8)', width: '70%'}}  classes={{h1: classes.responsiveText}}> 
+          Tra i nostri amici contiamo la Onlus Sardinien Hunde, che da anni si impegna per la cura e la protezione dei cagnolini della Sardegna.
+              </Typography>
+        </ParallaxLayer> 
 
          <ParallaxLayer
-          offset={2}
+          offset={5}
           speed={-0}
           style={{
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
           }}
-          onClick={() => parallax.current.scrollTo(0)}>
-          <img src={littleDog} style={{ width: '20%' }} />
+          onClick={() => parallax.current.scrollTo(3)}>
+          <img src={littleDog} className={classes.responsiveImage} />
         </ParallaxLayer> 
       </Parallax>
     </div>
