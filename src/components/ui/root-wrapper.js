0e4ba2ef6cb5f4a9051b424f1c4b87c7da2bc846
muni <booscarface@gmail.com>
2023-03-11 19:24:56 +0100

    added jss provider

--- src/components/ui/root-wrapper.js
+++ src/components/ui/root-wrapper.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { ThemeProvider } from '@material-ui/core/styles';
+import {createGenerateId, JssProvider} from 'react-jss';
+
 import theme from './theme'
+
+const generateId = createGenerateId();
 
 const RootWrapper = ( { element } ) => {
     return (
-        <ThemeProvider theme={theme} injectFirst>
+        <JssProvider generateId={generateId}>
+            <ThemeProvider theme={theme}>
             {element}
-        </ThemeProvider>
+            </ThemeProvider>
+        </JssProvider>
+        
     )
 }
 
